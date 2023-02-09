b90ff0b2124cdd66d848889e68e2f41dc3f3f67b
manuella <manuelladcsilvacruz@gmail.com>
2023-02-09 17:19:02 -0300

    docs: correct duplicated html_theme_options config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,19 +66,16 @@ html_theme_options = {
         },
     ],
     "collapse_navigation": True,
-    "navigation_depth": 2
+    "navigation_depth": 2,
+    "logo": {
+      "image_light": "_static/logo.png",
+      "image_dark": "_static/logo.png",
+   }
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-html_theme_options = {
-   "logo": {
-      "image_light": "_static/logo.png",
-      "image_dark": "_static/logo.png",
-   }
-}
 
 source_suffix = [".rst"]
