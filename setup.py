d42e0478bf668e1575d0add5db18fade03794254
Jonathan Carvalho <jonathan.carvalho@datarisk.io>
2023-03-13 11:27:34 -0300

    Add Manifest file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 MODULE_NAME = 'neomaril-codex'
 MODULE_NAME_IMPORT = 'neomaril_codex'
 REPO_NAME = 'mlops-neomaril-codex'
-MODULE_VERSION='1.0.0'
+MODULE_VERSION='1.0.0c'
 
 def requirements_from_pip(filename='requirements.txt'):
     with open(filename, 'r') as pip:
