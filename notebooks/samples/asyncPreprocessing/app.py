642f559b6e0e9bb67c1d878363cb46f10bf02979
Guilherme de Paula Cardoso <42394263+gkardoso@users.noreply.github.com>
2023-07-31 18:32:47 -0300

    Update in pre processing examples

--- notebooks/samples/asyncPreprocessing/app.py
+++ notebooks/samples/asyncPreprocessing/app.py
@@ -1,11 +1,22 @@
 import pandas as pd
 
 def process(data_path):
-    X = pd.read_csv(data_path+'/input.csv')
-    df = X.copy()
+    df = pd.read_csv(data_path+'/input.csv')
+
+    parameters_to_keep = [
+        'mean_radius', 'mean_texture', 'mean_perimeter', 'mean_area',
+        'mean_smoothness', 'mean_compactness', 'mean_concavity', 'mean_concave_points',
+        'mean_symmetry', 'mean_fractal_dimension', 'radius_error', 'texture_error',
+        'perimeter_error', 'area_error', 'smoothness_error', 'compactness_error',
+        'concavity_error', 'concave_points_error', 'symmetry_error', 'fractal_dimension_error',
+        'worst_radius', 'worst_texture', 'worst_perimeter', 'worst_area', 'worst_smoothness',
+        'worst_compactness', 'worst_concavity', 'worst_concave_points', 'worst_symmetry',
+        'worst_fractal_dimension'
+    ]
+
+    final_df = df[parameters_to_keep]
 
     csv_filename = data_path + '/output.csv'
-
-    df.to_csv(csv_filename, index=False)
+    final_df.to_csv(csv_filename, index=False)
 
     return csv_filename
