118f2df118ac36f9a242bbbfa368ea92a9ed02e3
manuella <manuelladcsilvacruz@gmail.com>
2023-10-19 11:36:22 -0300

    docs: model and train code example enhacement and formatting

--- notebooks/samples/asyncModel/app.py
+++ notebooks/samples/asyncModel/app.py
@@ -10,12 +10,12 @@ def score(data_path, model_path):# O nome da função (score) é que deve ser pa
     da aplicação do modelo para os dados reais. 
     
     Na função o usuário pode usar variáveis de ambiente carregadas a partir de um arquivo .env,
-    como exemplificado no código nas linhas 37-40.
+    como exemplificado no código nas linhas 35-38.
     O Neomaril traz a opções de carregar duas variáveis de ambiente:
     modelFileName : str
-        Que contém o nome do arquivo do modelo treinado (exemplo nas linhas 42-43)
+        Que contém o nome do arquivo do modelo treinado (exemplo nas linhas 40-41)
     inputFileName : str
-        Que contém o nome do arquivo da base de dados que foi feito upload (exemplo nas linhas 42-43)
+        Que contém o nome do arquivo da base de dados que foi feito upload (exemplo nas linhas 47-48)
 
     Parâmetros
     ---------
@@ -27,7 +27,9 @@ def score(data_path, model_path):# O nome da função (score) é que deve ser pa
     Retorno
     -------
     str:
-        O caminho com os arquivos resultantes da execução do modelo
+        O caminho com o(s) arquivo(s) com os resultados da execução do modelo.
+        Neste exemplo, retornamos a tabela com os inputs e a predição e a probabilidade para cada 
+        linha da tabela.
     """
 
     ## Variáveis de ambiente carregadas de um arquivo fornecido pelo usuário no campo 'env'
@@ -35,21 +37,29 @@ def score(data_path, model_path):# O nome da função (score) é que deve ser pa
     # if my_var is None:
     #    raise Exception("Could not find `env` variable")
 
-    ## Variável de ambiente carregada do Neomaril com nome do arquivo do modelo (usado em alternativa a linha 40)
+    ## Variável de ambiente carregada do Neomaril com nome do arquivo do modelo (usado em alternativa a linha 44)
     # with open(base_path+os.getenv('modelFileName'), 'rb') as f:
+
+    # Usado para construir o modelo a ser executado com base no arquivo de modelo passado como parâmetro
     with open(model_path+"/model.pkl", 'rb') as f:
         model = load(f)
 
-    ## Variável de ambiente carregada do Neomaril com nome da base de dados (usado em alternativa a linha 45)
+    ## Variável de ambiente carregada do Neomaril com nome da base de dados (usado em alternativa a linha 51)
     # X = pd.read_csv(data_path+'/'+os.getenv('inputFileName'))
+
+    # Carrega os dados da base de entrada do arquivo para um DataFrame
     X = pd.read_csv(data_path+"/dados.csv")
-    df = X.copy()
 
-    df['proba'] = model.predict_proba(X)[:,1]
-    df['pred'] = model.predict(X)
+    df = X.copy()   # Cria uma cópia do DataFrame com os dados de entrada
 
+    df['proba'] = model.predict_proba(X)[:,1]   # Calcula a predição de cada entrada da tabela de dados
+    df['pred'] = model.predict(X)               # Calcula a probabilidade de cada entrada da tabela de dados
+
+    # Cria o caminho com o nome do arquivo de output, nesse caso 'output.csv' 
     output = data_path+'/output.csv'
 
+    # Transforma o DataFrame, com a predição e a probabilidade, para csv colocando no arquivo do caminho do output
     df.to_csv(output, index=False)
 
+    # Retorna o caminho com o arquivo com os resultados da execução do modelo
     return output
--- notebooks/samples/syncModel/app.py
+++ notebooks/samples/syncModel/app.py
@@ -11,9 +11,9 @@ def score(data, base_path): # O nome da função (score) é que deve ser passado
     da aplicação do modelo para os dados reais. 
     
     Na função o usuário pode usar variáveis de ambiente carregadas a partir de um arquivo .env,
-    como exemplificado no código nas linhas 37-40.
+    como exemplificado no código nas linhas 39-42.
     Caso não queira deixar o nome do arquivo com as variáveis do modelo fixo, o Neomaril carrega 
-    o nome desse arquivo na variável de ambiente (exemplo nas linhas 42-43):
+    o nome desse arquivo na variável de ambiente (exemplo nas linhas 44-45):
     modelFileName : str
         Que contém o nome do arquivo do modelo treinado
 
@@ -26,12 +26,14 @@ def score(data, base_path): # O nome da função (score) é que deve ser passado
 
     Retorno
     -------
-    dict | str no formato JSON:
-        Um dicionário ou string no format de um JSON válido contendo as seguintes chaves
-        pred: int
-            Valor de predição do modelo
-        proba: int 
-            Probabilidade da ocorrência de 'a'
+    dict | list dict| str no formato JSON:
+        Um dicionário, lista de dicionários ou string no format de um JSON válido contendo 
+        as chaves e os valores para os resultados de saída do modelo.
+        Neste exemplo, usamos a chaves:
+            pred: int
+                Valor de predição do modelo
+            proba: int 
+                Probabilidade da pertencer a classe 'a'
     """
 
     ## Variáveis de ambiente carregadas de um arquivo fornecido pelo usuário no campo 'env'
@@ -39,14 +41,15 @@ def score(data, base_path): # O nome da função (score) é que deve ser passado
     # if my_var is None:
     #    raise Exception("Could not find `env` variable")
 
-    ## Variável de ambiente carregada do Neomaril com nome do arquivo do modelo (usado em alternativa a linha 44)
+    ## Variável de ambiente do Neomaril com nome do arquivo do modelo (usado em alternativa a linha 48)
     # with open(base_path+os.getenv('modelFileName'), 'rb') as f:
 
     # Usado para construir o modelo a ser executado com base no arquivo de modelo passado como parâmetro
     with open(base_path+"/model.pkl", 'rb') as f:
         model = load(f)
 
+    # Constrói o DataFrame com os dados de entrada
     df = pd.DataFrame(data=json.loads(data), index=[0])
     
-    # Retorna os resultados da execução do modelo segundo os parâmetros esperados pelo Neomaril
+    # Retorna os resultados da execução do modelo como um dicionário
     return {"pred": int(model.predict(df)), "proba": float(model.predict_proba(df)[0,1])}
