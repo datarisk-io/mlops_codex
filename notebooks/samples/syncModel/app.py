dcd6e0a2505e637f1ce6eef3d4868970e3fc5730
Luis Henrique <henrique.santos.lhls@gmail.com>
2024-08-06 11:38:33 -0300

    fix: update sync model example. It was throwing a vulnerability error

---
+++ notebooks/samples/syncModel/app.py
@@ -0,0 +1,12 @@
+import json
+from cloudpickle import load
+import pandas as pd
+
+
+def score(data, base_path):
+    with open(base_path+"/model.pkl", 'rb') as f:
+        model = load(f)
+
+    df = pd.DataFrame(data=json.loads(data), index=[0])
+    
+    return {"pred": int(model.predict(df)), "proba": float(model.predict_proba(df)[0,1])}
