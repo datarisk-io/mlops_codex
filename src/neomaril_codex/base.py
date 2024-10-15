7c6f0ea08b80d9fc8609dff94a8fb2f4ce6d0597
henrique-lh <henrique.santos.lhls@gmail.com>
2024-10-15 14:54:08 -0300

    refactor: apply pep 20 - The Zen of Python: Explicit is better than implicit.

--- src/neomaril_codex/base.py
+++ src/neomaril_codex/base.py
@@ -7,9 +7,9 @@ import requests
 from dotenv import load_dotenv
 from loguru import logger
 
-from neomaril_codex.__utils import *
-from neomaril_codex.exceptions import *
-
+from neomaril_codex.__utils import refresh_token, parse_url, try_login
+from neomaril_codex.exceptions import ExecutionError, InputError, AuthenticationError, ModelError, ServerError
+from neomaril_codex.__model_states import ModelExecutionState
 
 class BaseNeomaril:
     """
