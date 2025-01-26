6f85e5c35579616bd0f9859e9e9f0feda17ade94
henrique-lh <henrique.santos.lhls@gmail.com>
2025-01-26 12:34:04 -0300

    - adds docstrings to validate_group_existence and validate_python_version functions for improved documentation - formats validate_group_existence to include clearer structure and argument explanation - ensures consistent newline handling at the end of the file

--- src/mlops_codex/validations.py
+++ src/mlops_codex/validations.py
@@ -1,7 +1,21 @@
 from mlops_codex.exceptions import GroupError, InputError
 from mlops_codex.base import BaseMLOpsClient
 
+
 def validate_group_existence(group: str, client_object: BaseMLOpsClient) -> bool:
+    """
+    Validates that the given group exists.
+
+    Args:
+        group (str): The name of the group.
+        client_object (BaseMLOpsClient): The client object that will be used to
+
+    Returns:
+        bool: True if the group exists
+
+    Raises:
+        GroupError: If the group does not exist
+    """
     groups = [g.get("Name") for g in client_object.list_groups()]
     if group in groups:
         return True
@@ -9,6 +23,15 @@ def validate_group_existence(group: str, client_object: BaseMLOpsClient) -> bool
 
 
 def validate_python_version(python_version: str) -> bool:
+    """
+    Validates that the Python version is valid.
+
+    Args:
+        python_version (str): The Python version to validate.
+
+    Returns:
+        bool: True if the Python version is valid, False otherwise.
+    """
     if python_version not in ["3.8", "3.9", "3.10"]:
         raise InputError(
             "Invalid python version. Available versions are 3.8, 3.9, 3.10"
