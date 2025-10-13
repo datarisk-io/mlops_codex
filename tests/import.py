7e5e6d72217663a4a1856c86135d0a224dba981d
henrique-lh <henrique.santos.lhls@gmail.com>
2025-10-13 09:28:18 -0300

    - remove pipeline import

--- tests/import.py
+++ tests/import.py
@@ -3,6 +3,5 @@ from mlops_codex.training import MLOpsTrainingClient
 from mlops_codex.datasources import MLOpsDataSourceClient
 from mlops_codex.preprocessing import MLOpsPreprocessingClient
 from mlops_codex.model import MLOpsModelClient
-from mlops_codex.pipeline import MLOpsPipeline
 
 print("All imports were successful")
