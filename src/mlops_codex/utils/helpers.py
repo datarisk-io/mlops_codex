efc1069bc538b539ea5a2fc62b4fbbb1e49b6a71
henrique-lh <henrique.santos.lhls@gmail.com>
2025-10-27 10:23:43 -0300

    - improve wait function - ruff

--- src/mlops_codex/utils/helpers.py
+++ src/mlops_codex/utils/helpers.py
@@ -3,13 +3,15 @@ from enum import StrEnum
 from typing import Callable
 
 
-def wait(f: Callable, valid_status: list[StrEnum], status_enum, **kwargs):
-    print("Waiting for ready", end='')
+def wait(
+    f: Callable, valid_status: list[StrEnum], status_enum, status_key: str, **kwargs
+) -> dict:
+    print('Waiting for ready', end='')
     current_status = None
+    response = {}
     while current_status not in valid_status:
         time.sleep(30)
         response = f(**kwargs).json()
         print('.', end='', flush=True)
-        current_status = status_enum(response['Status'])
-    print()
-
+        current_status = status_enum(response[status_key])
+    return response.json()
